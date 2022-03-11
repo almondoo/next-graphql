61e98ead5b51d80e178af960ebcb2f9160b3d0c0
tsubasa111 <>
2022-03-11 11:21:24 +0900

    バックエンドのフロントの部分は必要ないので削除

--- laravel/resources/views/components/alert.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="alert--component {{ $type }}">
-  {{ $slot }}
-</div>
--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -11,4 +11,4 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').sass('resources/css/union.scss', 'public/css/app.css').version();
+mix.js('resources/js/app.js', 'public/js').sass('resources/css/app.css', 'public/css/app.css').version();
